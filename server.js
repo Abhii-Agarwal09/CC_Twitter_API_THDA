a504a0fb9e618bed587db3662a742cf2fa90ba88
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2022-04-22 23:44:54 +0530

    port issue fix

--- server.js
+++ server.js
@@ -150,4 +150,5 @@ app.get('/users', (req, res) => {
     getUsersByUsername(req, res);
 });
 
-app.listen(3000, () => console.log('listening on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
